e6e9509a1edbdd40d919e37b0edfca1dea014726
Valeriy <goldaboss@yandex.ru>
2023-09-21 20:20:06 +0300

    add images and font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgOptimizedImage } from '@angular/common';
+import { MainComponent } from './components/main/main.component';
+import { MatIconModule } from '@angular/material/icon';
+import { OrderComponent } from './components/order/order.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    BannerComponent,
+    MainComponent,
+    OrderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    NgOptimizedImage,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
